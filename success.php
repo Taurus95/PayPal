29dbf24e6323c4a245c753d85f6dd3574965aac4
andres chacon <andres@Taurus>
2019-06-10 18:54:29 -0400

    session to the top

--- success.php
+++ success.php
@@ -1,9 +1,6 @@
 <<?php
 session_start();
-?>
-<!--DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"-->
-<html xmlns="http://www.w3.org/1999/xhtml">
-<?php
+
 require('./env.php');
 require('./db/Connection.php');
 
@@ -47,6 +44,8 @@ if(sizeof($data)!=0){
 }
 
 ?>
+<!--DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"-->
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <title>Pagos</title>
