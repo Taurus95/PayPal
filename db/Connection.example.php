5ab285af5998aaa03989bc8c50efe5c8376bfce4
andres chacon <andres@Taurus>
2019-06-11 12:58:49 -0400

    examples

---
+++ db/Connection.example.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Connection extends PDO {
+
+    protected $host = "localhost";
+    protected $dbname = "andresc_paypal_payments";
+    protected $username = "root";
+    protected $password = "root19520";
+
+    function __construct() {
+        try {
+            parent::__construct('mysql:host='.$this->host.';dbname='.$this->dbname, $this->username, $this->password);
+            parent::setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (Exception $ex) {
+            die('The database doesn"t exist');
+        }
+    }
+}
