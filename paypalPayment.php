fddd133d0c20e81c81afb2daf49856ab5c4763b8
andres chacon <andres@Taurus>
2019-06-11 13:08:45 -0400

    change to eur

--- paypalPayment.php
+++ paypalPayment.php
@@ -1,10 +1,10 @@
 <?php
 //session for testing
 session_start();
-$_SESSION["total_amount"]=10;
+$_SESSION["total_amount"]=12;
 $_SESSION["item_name"]="Un par de weas en euros";
 $_SESSION["item_number"]=rand(1,1000);
-$_SESSION["currency_code"]="USD";
+$_SESSION["currency_code"]="EUR";
 //fin
 ?>
 <!--DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"-->
